d7ff8f1853e2222f808d746acbf2d0c0436325b6
Zvika Berkovich <zvika0556705015@gmail.com>
2023-08-21 15:32:32 +0300

    the first connection between backEnd to the frontEnd

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import productsRouter from './api/products/router.products.js';
 import usersRuoter from './api/users/router.users.js'
 import morgan from 'morgan';
+import cors from 'cors';
 
 const port = 8200;
 const app = express();
@@ -9,6 +10,18 @@ const app = express();
 app.use(express.json());
 
 app.use(morgan('dev'))
+
+
+// const corsOptions = {
+//     origin: 'C:\\Users\\ZVIKA\\Documents\\project2\\Dom Project\\shop.js', // Specify the allowed origin
+//     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],     // Specify allowed HTTP methods
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+// };
+
+// app.use(cors(corsOptions)); 
+
+app.use(cors())
+
 
 app.use('/api/products', productsRouter)
 
