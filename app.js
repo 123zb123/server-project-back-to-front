de39975cf5c7958d90ba380cff37633ace35b41e
Zvika Berkovich <zvika0556705015@gmail.com>
2023-08-24 12:39:12 +0300

    cors options

--- app.js
+++ app.js
@@ -12,17 +12,21 @@ app.use(express.json());
 app.use(morgan('dev'))
 
 
+const corsOptions = {
+    origin: 'https://example-store.netlify.app', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
 
-app.use(cors())
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('API Deployed 🚀')
-  })
+})
 
 app.use('/api/products', productsRouter)
-
 app.use('/api/users', usersRuoter)
-
 
 app.listen(port, () => {
     console.log(`Server is up and running on port: ${port}`);
